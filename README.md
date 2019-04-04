25816ca84a4858677d891425eacb65e832466efb
Kosta Korenkov <kosta@whoot.me>
2019-04-04 10:14:50 +0300

    feat: add setValidator script

--- README.md
+++ README.md
@@ -9,13 +9,36 @@ SPENT_HASH=0x6f9632e11fafe13f8703ddf62c13266000e7ba5561cd6a4d584c85ba002f181f EX
 
 ## Machine gun
 Script to add more transactions to the given network (usually to incite the next period submission). 
+
+**Env params:**
 - NODE_URL - JSON RPC endpoint of the Leap node
 - PRIV_KEY - private key for the plasma account to machine gun from. This account should be funded with 100 LEAP cents.
 UTXO from this account will be spend to itself, so it is good for any number of transactions
 - NUM - Number of trnsactions to send. Default is 1
   
-Example:
+**Example:**
  
 ```
 NUM=5 NODE_URL=http://localhost:8645 PRIV_KEY=0xbd54b17c48ac1fc91d5ef2ef02e9911337f8758e93c801b619e5d178094486cc node scripts/machineGun.js
+```
+
+## set validator
+
+Sets given validator slotId on the given network. Changes epochLength if needed.
+
+**Restrictions:**
+- network should be governed by MinGov
+- proposal time for MinGov should be 0
+
+**Env params:**
+- NODE_URL    - JSON RPC endpoint of the Leap node
+- PRIV_KEY    - private key for ethereum account owning the network governance
+- SLOT        - (optional) validator slotId to set. Defaults to 0
+- TENDER_ADDR - (optional) tendermint validator address. If not specified, will be taken from the node
+- VAL_ADDR    - (optional) validator address. If not specified, will be taken from the node
+
+**Example:**
+
+```
+SLOT=1 PRIV_KEY=0xe0fb6e8a745d9f266410b51a9b2abb4f63ef7e6cd55a8328a76d095669088be2 node scripts/setValidator.js
 ```
---
+++ scripts/setValidator.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2018-present, Leap DAO (leapdao.org)
+ *
+ * This source code is licensed under the Mozilla Public License Version 2.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint-disable no-await-in-loop, no-console */
+
+
+/**
+ * Sets given validator slotId on the given network. Changes epochLength if needed.
+ * 
+ * Restrictions:
+ * - network should be governed by MinGov
+ * - proposal time for MinGov should be 0
+ * 
+ * Env params:
+ * NODE_URL    - JSON RPC endpoint of the Leap node
+ * PRIV_KEY    - private key for ethereum account owning the network governance
+ * SLOT        - (optional) validator slotId to set. Defaults to 0
+ * TENDER_ADDR - (optional) tendermint validator address. If not specified, will be taken from the node
+ * VAL_ADDR    - (optional) validator address. If not specified, will be taken from the node
+ * 
+ * Example: SLOT=1 PRIV_KEY=0xe0fb6e8a745d9f266410b51a9b2abb4f63ef7e6cd55a8328a76d095669088be2 node scripts/setValidator.js
+ */
+
+const ethers = require('ethers');
+const { getRootNetworkProvider } = require('./utils');
+
+const operatorAbi = require('../abis/operator');
+const governanceAbi = require('../abis/minGov');
+
+const nodeUrl = process.env.NODE_URL || 'http://localhost:8645';
+
+const slotId = parseInt(process.env.SLOT) || 0;
+const privKey = process.env.PRIV_KEY;
+
+const getValidatorDetails = async (plasma) => {
+  if (process.env.TENDER_ADDR) {
+    return { 
+      ethAddress: process.env.VAL_ADDR, 
+      tendermintAddress: process.env.TENDER_ADDR,
+    };
+  } else {
+    return await plasma.send('validator_getAddress', []);
+  }
+};
+
+async function run() {
+  const plasma = new ethers.providers.JsonRpcProvider(nodeUrl);
+  const nodeConfig = await plasma.send('plasma_getConfig', []);
+  const root = new ethers.providers.JsonRpcProvider(getRootNetworkProvider(nodeConfig));
+  const wallet = new ethers.Wallet(privKey, root);
+
+  const { operatorAddr } = nodeConfig;
+
+  const operator = new ethers.Contract(operatorAddr, operatorAbi, wallet);
+  const governance = new ethers.Contract(await operator.admin(), governanceAbi, wallet);
+  
+  const epochLength = (await operator.epochLength()).toNumber();
+  console.log('Current epoch length', epochLength);  
+  if (epochLength <= slotId) {
+    console.log(`Setting epoch length to ${slotId + 1}..`);
+    const data = operator.interface.functions.setEpochLength.encode([slotId + 1])
+    tx = await governance.propose(operatorAddr, data).then(tx => tx.wait());
+  }
+  
+  console.log(`Setting slot ${slotId}..`);
+  
+  const { ethAddress, tendermintAddress } = await getValidatorDetails(plasma); 
+  const overloadedSlotId = `${operatorAddr}00000000000000000000000${slotId}`;
+  await governance.setSlot(overloadedSlotId, ethAddress, `0x${tendermintAddress}`);
+
+  console.log(`Funding validator account..`);
+  await wallet.sendTransaction({
+    to: ethAddress,
+    value: ethers.utils.parseEther('1.0')
+  });
+
+  tx = await governance.finalize().then(tx => tx.wait());
+  console.log('✅ Done');
+  console.log('Current epoch length', (await operator.epochLength()).toNumber());
+  console.log(`Slot ${slotId}`, await operator.slots(slotId));
+}
+
+run();
