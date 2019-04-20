58d0f7e6d2d67792ad458a6e410cc7f7f6b34095
eshavkun <dmoroz.thrash@gmail.com>
2019-04-20 23:22:47 +0200

    Network Migration scripts (#9)
    * Migration scripts added
    
    * Migration scripts added
    
    * Check if address if funded before dispensing
    
    * use process.argv to get URL

---
+++ scripts/migration/compareBalances.js
@@ -0,0 +1,33 @@
+const JSBI = require('jsbi');
+const ethers = require('ethers');
+const fs = require('fs');
+const csv = require('csv-parser');
+const { getBalance } = require('./helpers');
+
+const nodeUrl = process.argv[2] ? process.argv[2] : "https://testnet-node.leapdao.org";
+const rpc = new ethers.providers.JsonRpcProvider(nodeUrl);
+const snapshot = './snapshot.csv';
+
+async function run() {
+    const balances = [];
+
+    fs.createReadStream(snapshot)  
+    .pipe(csv())
+    .on('data', (row) => {
+        balances.push(row);
+    })
+    .on('end', async () => {
+        let balance; 
+        for(let i = 0; i < balances.length; i++) {
+            console.log('Checking ballance of', balances[i].Address);
+            balance = await getBalance(balances[i].Address, rpc);
+            if (String(balance) === balances[i].Balance) {
+                console.log('   OK');
+            } else {
+                console.log('   Mismatch! Expected:', balances[i].Balance, 'actual: ', String(balance));
+            }
+        }        
+    });  
+}
+
+run();
