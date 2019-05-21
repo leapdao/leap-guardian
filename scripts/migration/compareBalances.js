7bcb561c2503b9ab869ddda05a94f3469ae5d5d5
Kosta Korenkov <kosta@whoot.me>
2019-05-21 10:01:57 +0300

    feat: output the result (PASS/FAIL) for compare balances

--- scripts/migration/compareBalances.js
+++ scripts/migration/compareBalances.js
@@ -1,39 +1,54 @@
-const ethers = require("ethers");
-const fs = require("fs");
-const csv = require("csv-parser");
-const { getBalance } = require("./helpers");
+const ethers = require('ethers');
+const fs = require('fs');
+const csv = require('csv-parser');
+const { getBalance } = require('./helpers');
 
 const nodeUrl = process.argv[2]
   ? process.argv[2]
-  : "https://testnet-node.leapdao.org";
+  : 'https://testnet-node.leapdao.org';
 
 const rpc = new ethers.providers.JsonRpcProvider(nodeUrl);
-const snapshot = "./snapshot.csv";
+const snapshot = './snapshot.csv';
+
+const readSnapshot = () =>
+  new Promise(resolve => {
+    const balances = [];
+    fs.createReadStream(snapshot)
+      .pipe(csv())
+      .on('data', row => {
+        balances.push(row);
+      })
+      .on('end', () => resolve(balances));
+  });
 
 async function run() {
-  const balances = [];
+  const balances = await readSnapshot();
 
-  fs.createReadStream(snapshot)
-    .pipe(csv())
-    .on("data", row => {
-      balances.push(row);
+  let match = true;
+
+  await Promise.all(
+    balances.map(async record => {
+      return getBalance(record.Address, rpc)
+        .then(balance => {
+          if (String(balance) === record.Balance) {
+            console.log(record.Address, 'OK');
+          } else {
+            match = false;
+            console.log(
+              record.Address,
+              'FAIL',
+              `Expected: ${record.Balance}, Actual: ${balance}`,
+            );
+          }
+        })
+        .catch(e => {
+          match = false;
+          console.log(record.Address, 'FAIL', e);
+        });
     })
-    .on("end", async () => {
-      balances.forEach(async record => {
-        const balance = await getBalance(record.Address, rpc);
-        if (String(balance) === record.Balance) {
-          console.log(record.Address, "   OK");
-        } else {
-          console.log(
-            record.Address,
-            "   Mismatch! Expected:",
-            record.Balance,
-            "actual: ",
-            String(balance)
-          );
-        }
-      });
-    });
+  );
+
+  console.log('Balance check:', match ? 'PASS' : 'FAIL (see above)');
 }
 
 run();
