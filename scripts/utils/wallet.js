6fc898d02b8a90b8298113844424cb78c98ff58e
Kosta Korenkov <kosta@whoot.me>
2019-06-27 15:12:00 +0300

    feat: add registerToken script (#18)
    * feat: add registerToken script
    
    * fix: do not print priv key
    
    * chore: update yarn.lock

--- scripts/utils/wallet.js
+++ scripts/utils/wallet.js
@@ -2,10 +2,9 @@ const ethers = require('ethers');
 const getRootNetworkProvider = require('./getRootNetworkProvider');
 
 module.exports = async (params = {}) => {
-  console.log(params);
   const nodeUrl = params.nodeUrl || process.env.NODE_URL || 'http://localhost:8645';
   const privKey = params.privKey || process.env.PRIV_KEY || '0x0000000000000000000ef2ef02e9911337f8758e93c801b619e5d178094486cc';
-  console.log(nodeUrl, privKey);
+  console.log('Leap node: ', nodeUrl);
   const plasmaWallet = new ethers.Wallet(
     privKey, 
     new ethers.providers.JsonRpcProvider(nodeUrl),
