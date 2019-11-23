7c0ae4c0a0cc0764f749bc01f9d1913681a63472
Kosta Korenkov <kosta@whoot.me>
2019-11-23 09:59:12 +0300

    fix: make printPeriods script support CAS periods

--- scripts/printPeriods.js
+++ scripts/printPeriods.js
@@ -7,19 +7,28 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
+ /**
+ * Watch for and print new periods on the root chain.
+ * 
+ * NODE_URL - JSON RPC endpoint of the Leap node
+ * 
+ * Example: NNODE_URL=http://localhost:8645 node scripts/printPeriods
+ */
+
 const ethers = require('ethers');
 
+const OPERATOR_ABI = require('./../abis/operatorAbi');
 const BRIDGE_ABI = require('./../abis/bridgeAbi');
 const NODE_URL = process.env.NODE_URL || 'https://mainnet-node1.leapdao.org';
 const provider = new ethers.providers.JsonRpcProvider(NODE_URL);
 
 let currentBlockNumber = 0;
 
-async function onNewHeight(height, root, tx) {
+async function onSubmission(blocksRoot, slotId, owner, casBitmap, periodRoot, tx) {
   const blocksPassed = currentBlockNumber - tx.blockNumber;
   const minutesPassed = ((blocksPassed * 16) / 60).toFixed(1);
 
-  console.log(`Ethereum block: ${tx.blockNumber} (~${minutesPassed} minutes ago) height: ${height.toString()} root: ${root}`);
+  console.log(`Ethereum block: ${tx.blockNumber} (~${minutesPassed} minutes ago) root: ${blocksRoot}`);
 }
 
 async function run() {
@@ -27,10 +36,11 @@ async function run() {
   console.log(config);
 
   const bridge = new ethers.Contract(config.bridgeAddr, BRIDGE_ABI, new ethers.providers.JsonRpcProvider(config.rootNetwork));
+  const operator = new ethers.Contract(config.operatorAddr, OPERATOR_ABI, new ethers.providers.JsonRpcProvider(config.rootNetwork));
   const tipHash = await bridge.tipHash();
 
-  bridge.on(bridge.filters.NewHeight(), onNewHeight);
-  currentBlockNumber = await bridge.provider.getBlockNumber();
+  operator.on(operator.filters.Submission(), onSubmission);
+  currentBlockNumber = await operator.provider.getBlockNumber();
 
   console.log(`tipHash: ${tipHash}`);
   console.log('fetching events since genesis...');
